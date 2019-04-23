2a6c622730153f895c9a1f7ca5b62050c541afaf
Jonathan Ferrer <jonathanferrer95@gmail.com>
2019-04-23 12:43:22 -0400

    Printed a greeting to the console

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         print("My name is Jonathan Ferrer")
+        print("How are you today?")
     }
 
 
